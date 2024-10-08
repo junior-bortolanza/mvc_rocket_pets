16a01e02d888cb7e8c3876b6693f44b7bebb592c
Junior Bortolanza <gbortolanzajr@gmail.com>
2024-10-08 14:23:58 -0300

    feat: Implementing person_creator_controller with tests

---
+++ src/controllers/person_creator_controller.py
@@ -0,0 +1,39 @@
+import re
+from src.models.sqlite.interfaces.people_repository import PeopleRepositoryInterface
+
+
+class PersonCreatorController:
+    def __init__(self, people_repository: PeopleRepositoryInterface ) -> None:
+        self.__people_repository = people_repository
+
+
+    def create(self, person_info: dict) -> dict:
+        first_name = person_info["first_name"]
+        last_name = person_info["last_name"]
+        age = person_info["age"]
+        pet_id = person_info["pet_id"]
+
+        self.__validate_first_name_and_last_name(first_name, last_name)
+        self.__insert_person_in_db(first_name, last_name, age, pet_id)
+        formatted_response = self.__format_response
+        return formatted_response
+
+
+    def __validate_first_name_and_last_name(self, first_name: str, last_name: str) -> None:
+        # Expressão Regular para caractere que nao são letras.
+        non_valid_caracteres = re.compile(r'[^a-Za-Z]')
+
+        if non_valid_caracteres.search(first_name) or non_valid_caracteres.search(last_name):
+            raise Exception("Nome da pessoa inválido!")
+
+    def __insert_person_in_db(self, first_name: str, last_name: str, age: int, pet_id: int) -> None:
+        self.__people_repository.insert_person(first_name, last_name, age, pet_id)
+
+    def __format_response(self, person_info: dict) -> dict:
+        return {
+            "data": {
+                "type": "Person",
+                "count": 1,
+                "attributes": person_info
+            }
+        }
